32281dd367ee7dffdd23dff198194aeb87ec5878
rstambach <rstambach@my.waketech.edu>
2017-03-09 22:14:02 -0500

    Updated SQL query to select only facilities with a schedule

--- LetsPlay/app/src/main/java/wtsc/letsplay10/Settings.java
+++ LetsPlay/app/src/main/java/wtsc/letsplay10/Settings.java
@@ -5,20 +5,20 @@ package wtsc.letsplay10;
  */
 
         import android.content.SharedPreferences;
-        import android.preference.PreferenceManager;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.view.KeyEvent;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.view.View.OnClickListener;
-        import android.view.View.OnKeyListener;
-        import org.apache.commons.validator.routines.EmailValidator;
-        import android.view.inputmethod.EditorInfo;
-        import android.view.inputmethod.InputMethodManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 
-        import com.google.gson.Gson;
+import com.google.gson.Gson;
+
+import org.apache.commons.validator.routines.EmailValidator;
 
 public class  Settings extends AppCompatActivity implements OnClickListener, OnKeyListener{
 
