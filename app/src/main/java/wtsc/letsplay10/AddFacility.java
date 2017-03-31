5dc5fd259870044fcb35a37658f4ed77cf8a01de
lbyoung1 <lbyoung1@my.waketech.edu>
2017-03-31 15:17:51 -0400

    Added a little bit to Alex's code.
    Linked buttons and text fields to variables.
    
    In addition to Alex's get location information method which uses geoCoder and current location, I Created another method to add a facility to the database from the user entered data.
    
    Edited onClick method slightly to reflect new button names.
    
    Note***  If we can't figure out how to use the radio buttons in conjunction with the create facility button...1. press radio button to choose "by address", 2. Enter data, 3. press submit.....we may have to ask users to enter data first then have button at the bottom so all variables will be filled by the time the user presses the button.

--- app/src/main/java/wtsc/letsplay10/AddFacility.java
+++ app/src/main/java/wtsc/letsplay10/AddFacility.java
@@ -1,35 +1,23 @@
 package wtsc.letsplay10;
 
-import android.content.Context;
-import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
-import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.view.View.OnClickListener;
-import android.view.View.OnKeyListener;
-import org.apache.commons.validator.routines.EmailValidator;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
-import android.content.SharedPreferences;
-import android.location.Geocoder;
-import android.location.Address;
+import android.widget.RadioButton;
+
+import com.google.android.gms.location.LocationListener;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.gson.Gson;
 /**
  * Created by a1995 on 3/20/2017.
  */
@@ -44,39 +32,52 @@ public class AddFacility extends AppCompatActivity implements
     dbGetFacilitiesList db_GetFacilitiesList;
     private Location myLastLocation;
 
-    public void onKey(View view, int keyCode, KeyEvent event){}
+    private EditText FacilityNameText2;     //top one for current location
+    private EditText FacilityNameText;      //bottom one for find by address
+    private EditText AddressText;
+    private EditText CityText;
+    private EditText StateText;
+    private EditText ZipText;
 
-    public void onClick (View v) {
-        switch (v.getId()) {
-            case R.id.myLocation:
-                this.latitude = myLastLocation.getLatitude();
-                this.longitude = myLastLocation.getLongitude();
-                try{
-                    findLocationInformation();
-                }
-                catch(IOException IOE)
-                {
-                    IOE.printStackTrace();
-                }
-                break;
-            case R.id.viewAll:
-                //<code></code>
-                break;
-            case R.id.fromMap:
+    private RadioButton clButton;           //current location radio button
+    private RadioButton addressButton;      //find by address button
+    private Button createFacilityButton;    //button at bottom to create facility
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_facility);
 
-                break;
+        //preferences = getSharedPreferences("userSettings", MODE_PRIVATE);
+        //String json = preferences.getString("User", "");
 
-        }
+        FacilityNameText2 = (EditText) findViewById(R.id.FacilityNameText2);
+        FacilityNameText = (EditText) findViewById(R.id.FacilityNameText);
+        AddressText = (EditText) findViewById(R.id.AddressText);
+        CityText = (EditText) findViewById(R.id.CityText);
+        StateText = (EditText) findViewById(R.id.StateText);
+        ZipText = (EditText) findViewById(R.id.ZipText);
+
+        clButton = (RadioButton) findViewById(R.id.clButton);
+        clButton.setOnClickListener(this);
+
+        addressButton = (RadioButton) findViewById(R.id.addressButton);
+        addressButton.setOnClickListener(this);
+
+        createFacilityButton = (Button) findViewById(R.id.submitButton);
+        createFacilityButton.setOnClickListener(this);
     }
 
     @Override
-    public void onLocationChanged(Location location) {
+    public void onLocationChanged(Location location)
+    {
         myLastLocation = location;
+
     }
 
-    public void findLocationInformation() throws IOException
-    {
+    //run this if user selects to add facility based on current location
+
+    public void findLocationInformation() throws IOException {
         Geocoder geocoder;
         List<Address> addressInformation;
         geocoder = new Geocoder(this, Locale.getDefault());
@@ -94,5 +95,61 @@ public class AddFacility extends AppCompatActivity implements
 
         newFacility = db_AddNewFacility.doInBackground(name + address + city + state + zip + Double.toString(latitude)
                 + Double.toString(longitude) + notes);
+
+    }
+
+
+    //run this if add by address is selected.  Adds info using data gathered from user
+
+    public void addLocationInformation() throws IOException {
+
+        String address = AddressText.getText().toString();
+        String city = CityText.getText().toString();
+        String state = StateText.getText().toString();
+        String zip = ZipText.getText().toString();
+        String name = FacilityNameText.getText().toString();
+        String notes = "";
+
+        db_AddNewFacility = new dbAddNewFacility(AddFacility.this);
+
+        newFacility = db_AddNewFacility.doInBackground(name + address + city + state + zip + Double.toString(latitude)
+                + Double.toString(longitude) + notes);
+
+    }
+
+
+    public void onKey(View view, int keyCode, KeyEvent event){}
+
+    /*              //could possibly add something like this to disable button until user enters something into field.
+                    if(FacilityNameText2.length() > 0) //if user has entered something in the fields, the function will work
+                {
+     */
+
+    public void onClick (View v) {
+        switch (v.getId())
+        {
+            case R.id.clButton:
+                    this.latitude = myLastLocation.getLatitude();
+                    this.longitude = myLastLocation.getLongitude();
+                    try {
+                        findLocationInformation();
+                    } catch (IOException IOE) {
+                        IOE.printStackTrace();
+                    }
+                break;
+            case R.id.addressButton:
+                this.latitude = myLastLocation.getLatitude();
+                this.longitude = myLastLocation.getLongitude();
+                try{
+                    addLocationInformation();
+                }
+                catch(IOException IOE)
+                {
+                    IOE.printStackTrace();
+                }
+                break;
+            //case R.id.fromMap:
+              //  break;
+        }
     }
 }
