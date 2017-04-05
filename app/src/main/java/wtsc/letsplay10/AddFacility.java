b36afd7ddbcadcc500205bab5d3cc935f70117e7
rstambach <rstambach@my.waketech.edu>
2017-04-04 22:58:23 -0400

    Add new schedule feature

--- app/src/main/java/wtsc/letsplay10/AddFacility.java
+++ app/src/main/java/wtsc/letsplay10/AddFacility.java
@@ -1,35 +1,18 @@
 package wtsc.letsplay10;
 
-import android.content.Context;
-import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.view.View.OnClickListener;
-import android.view.View.OnKeyListener;
-import org.apache.commons.validator.routines.EmailValidator;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
-import android.content.SharedPreferences;
-import android.location.Geocoder;
-import android.location.Address;
+
+import com.google.android.gms.location.LocationListener;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
-
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.gson.Gson;
 /**
  * Created by a1995 on 3/20/2017.
  */
@@ -47,27 +30,27 @@ public class AddFacility extends AppCompatActivity implements
     public void onKey(View view, int keyCode, KeyEvent event){}
 
     public void onClick (View v) {
-        switch (v.getId()) {
-            case R.id.myLocation:
-                this.latitude = myLastLocation.getLatitude();
-                this.longitude = myLastLocation.getLongitude();
-                try{
-                    findLocationInformation();
-                }
-                catch(IOException IOE)
-                {
-                    IOE.printStackTrace();
-                }
-                break;
-            case R.id.viewAll:
-                //<code></code>
-                break;
-            case R.id.fromMap:
+ //       switch (v.getId()) {
+ //           case R.id.myLocation:
+ //               this.latitude = myLastLocation.getLatitude();
+ //               this.longitude = myLastLocation.getLongitude();
+ //               try{
+ //                   findLocationInformation();
+ //               }
+ //               catch(IOException IOE)
+//                {
+ //                   IOE.printStackTrace();
+ //               }
+ //               break;
+ //           case R.id.viewAll:
+ //               //<code></code>
+ //               break;
+ //           case R.id.fromMap:
 
 
-                break;
+ //               break;
 
-        }
+ //       }
     }
 
     @Override
@@ -90,7 +73,7 @@ public class AddFacility extends AppCompatActivity implements
         String name = addressInformation.get(0).getFeatureName(); // Only if available else return NULL
         String notes = "";
 
-        db_AddNewFacility = new dbAddNewFacility(AddFacility.this);
+//        db_AddNewFacility = new dbAddNewFacility(AddFacility.this);
 
         newFacility = db_AddNewFacility.doInBackground(name + address + city + state + zip + Double.toString(latitude)
                 + Double.toString(longitude) + notes);
