b7a14812ab079c408f38efaad5bdc0a6d64d5244
rstambach <rstambach@my.waketech.edu>
2017-04-11 17:38:39 -0400

    Add filtering for mySchedules and allSchedules

--- app/src/main/java/wtsc/letsplay10/dbGetMySchedulesMarkers.java
+++ app/src/main/java/wtsc/letsplay10/dbGetMySchedulesMarkers.java
@@ -55,12 +55,14 @@ public class dbGetMySchedulesMarkers extends AsyncTask<UserBounds,String,List<Ma
                     LatLng sw = params[0].getBounds().southwest;
                     LatLng ne = params[0].getBounds().northeast;
 
-                    query = "select f.[facility_ID], f.[Name] AS fName, f.Lat, f.Lng, f.[Address1],f.[Address2]" +
-                            ",f.[City],f.[State],f.[Zip],f.[Lat],f.[Lng],f.[Notes]"+
-                            ",s.[schedule_ID]"+
-                            " from [facility] f join [schedule] s "+"" +
-                            "on f.[facility_ID] = s.[Facility_ID]"+
-                            " WHERE "+
+                    query = "select f.[facility_ID], f.[Name] AS fName, f.Lat, f.Lng, f.[Address1],f.[Address2]"+
+                            ",f.[City],f.[State],f.[Zip],f.[Lat],f.[Lng],f.[Notes],s.[schedule_ID], "+
+                            "t.[Sports_Name],t.[Sports_Icon] "+
+                            "FROM [schedule] s "+
+                            "JOIN facility as f on f.Facility_ID = s.Facility_ID "+
+                            "JOIN SportsType as t on t.SportsType_ID = s.SportsType_ID "+
+                            "JOIN UserSchedule as u on u.Schedule_ID = s.Schedule_ID "+
+                            "WHERE "+
                             "[Lat] > " + sw.latitude + " AND [Lat] < " + ne.latitude + " AND " +
                             "[Lng] > " + sw.longitude + " AND [Lng] < " + ne.longitude + " AND " +
                             "[User_ID] = " + params[0].getUser().getID() +
@@ -74,16 +76,16 @@ public class dbGetMySchedulesMarkers extends AsyncTask<UserBounds,String,List<Ma
                 MarkerOptions mo;
 
                 while (rs.next()) {
-                    String n = rs.getString("fName") + rs.getString("Sports_Name");
+                    String n = rs.getString("fName") + " - " + rs.getString("Sports_Name");
                     LatLng LL = new LatLng(rs.getDouble("Lat"),rs.getDouble("Lng"));
                     Blob bl = rs.getBlob("Sports_Icon");
-                    int i = (int) bl.length();
-                    byte[] blAsBytes = bl.getBytes(1,i);
-                    Bitmap bm = BitmapFactory.decodeByteArray(blAsBytes,0,blAsBytes.length);
                     mo = new MarkerOptions();
                     mo.title(n);
                     mo.position(LL);
-                    if(bl != null){
+                    if(bl != null) {
+                        int i = (int) bl.length();
+                        byte[] blAsBytes = bl.getBytes(1, i);
+                        Bitmap bm = BitmapFactory.decodeByteArray(blAsBytes, 0, blAsBytes.length);
                         mo.icon(BitmapDescriptorFactory.fromBitmap(bm));
                     }
                     markersOList.add(mo);
